bfd4e124c85d91cefdff57fa492248a5403caae2
Student <student@Makerss-MacBook-Air.local>
2019-12-16 15:50:31 +0000

    structuring the controller

--- app.rb
+++ app.rb
@@ -3,7 +3,49 @@ require 'sinatra'
 class MakersBnb < Sinatra::Base
 
   get '/' do
-    'Hello world'
+    #all listings
+    #sign up button
+    #add listing
+  end
+
+  get '/sign-up' do
+    #form
+    #submit button
+  end
+
+  post '/sign-up' do
+    #user details (params)
+    #redirect to '/'
+  end
+
+  get '/new' do
+    #form
+    #submit
+  end
+
+  post '/new' do
+    #listing details (params)
+    #redirect to '/listing_id'
+  end
+
+  get '/listing-id' do
+    #details of listing
+    #book button should redirect to '/listing-id/book'
+  end
+
+  get '/listing-id/book' do
+    #form
+    #submit button
+  end
+
+  post '/listing-id/book' do
+    #booking details
+    #redirect to booking confirmation
+  end
+
+  get '/listing-id/confirmation' do
+    #booking details
+    #homepage button
   end
 
   run! if app_file == $0
