7d68dcde57588d2be77d5109f82f8974ae184938
Ellie <elliemmanifold@gmail.com>
2019-12-17 11:44:08 +0000

    Add owner_id

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,8 +1,9 @@
 class Listing
 
-  attr_reader :title, :address, :description, :price
+  attr_reader :owner_id, :title, :address, :description, :price
 
-  def initialize(title:, address:, description:, price:)
+  def initialize(owner_id:, title:, address:, description:, price:)
+    @owner_id = owner_id
     @title = title
     @address = address
     @description = description
@@ -17,19 +18,19 @@ class Listing
     end
     rows = connection.exec('SELECT * FROM listings')
     rows.map do |listing|
-      Listing.new(title: listing['title'], address: listing['address'], description: listing['description'], price: listing['price'])
+      Listing.new(owner_id: listing['owner_id'], title: listing['title'], address: listing['address'], description: listing['description'], price: listing['price'])
     end
   end
 
-  def self.create(title:, address:, description:, price:)
+  def self.create(owner_id:, title:, address:, description:, price:)
     if ENV['RACK_ENV'] = 'test'
       connection = PG.connect(dbname: "makersbnb_test")
     else
       connection = PG.connect(dbname: "makersbnb")
     end
 
-    result = connection.exec("INSERT INTO listings(address, description, price, title) VALUES('#{address}', '#{description}', '#{price}', '#{title}') RETURNING address, description, price, title;")
-    Listing.new(title: result[0]['title'], address: result[0]['address'], description: result[0]['description'], price: result[0]['price'])
+    result = connection.exec("INSERT INTO listings(owner_id, address, description, price, title) VALUES('#{owner_id}', '#{address}', '#{description}', '#{price}', '#{title}') RETURNING owner_id, address, description, price, title;")
+    Listing.new(owner_id: result[0]['owner_id'], title: result[0]['title'], address: result[0]['address'], description: result[0]['description'], price: result[0]['price'])
   end
 
 end
--- spec/unit/listing_spec.rb
+++ spec/unit/listing_spec.rb
@@ -6,9 +6,9 @@ describe "Listing" do
 
   describe ".all" do
     it "should display the listings" do
-      listing = Listing.create(title: 'test_title_1', address: 'test_address_1', description: 'test_description_1', price: 'test_price_1')
-      Listing.create(title: 'test_title_2', address: 'test_address_2', description: 'test_description_2', price: 'test_price_2')
-      Listing.create(title: 'test_title_3', address: 'test_address_3', description: 'test_description_3', price: 'test_price_3')
+      listing = Listing.create(owner_id: 001, title: 'test_title_1', address: 'test_address_1', description: 'test_description_1', price: 'test_price_1')
+      Listing.create(owner_id: 002, title: 'test_title_2', address: 'test_address_2', description: 'test_description_2', price: 'test_price_2')
+      Listing.create(owner_id: 003, title: 'test_title_3', address: 'test_address_3', description: 'test_description_3', price: 'test_price_3')
 
       listings = Listing.all
 
@@ -21,7 +21,7 @@ describe "Listing" do
 
   describe ".create" do
     it "should create a new instance of listing and add it to the db" do
-      listing = Listing.create(title: 'test_title', address: 'test_address_1', description: 'test_description', price: 'test_price')
+      listing = Listing.create(owner_id: 001, title: 'test_title', address: 'test_address_1', description: 'test_description', price: 'test_price')
 
       expect(listing).to be_a Listing
       expect(listing.title).to eq 'test_title'
