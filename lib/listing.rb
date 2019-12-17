76fa1fffb5b6b783087ea1bb1d4cfe52a27f0bdf
Ellie <elliemmanifold@gmail.com>
2019-12-17 11:29:11 +0000

    Create a create method on listing class

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,4 +1,13 @@
-class Listing 
+class Listing
+
+  attr_reader :title, :address, :description, :price
+
+  def initialize(title:, address:, description:, price:)
+    @title = title
+    @address = address
+    @description = description
+    @price = price
+  end
 
   def self.all
     if ENV['RACK_ENV'] = 'test'
@@ -8,9 +17,19 @@ class Listing
     end
     rows = connection.exec('SELECT * FROM listings')
     rows.map do |listing|
-      listing["address"]
+      Listing.new(title: listing['title'], address: listing['address'], description: listing['description'], price: listing['price'])
+    end
+  end
+
+  def self.create(title:, address:, description:, price:)
+    if ENV['RACK_ENV'] = 'test'
+      connection = PG.connect(dbname: "makersbnb_test")
+    else
+      connection = PG.connect(dbname: "makersbnb")
     end
 
-    
+    result = connection.exec("INSERT INTO listings(address, description, price, title) VALUES('#{address}', '#{description}', '#{price}', '#{title}') RETURNING address, description, price, title;")
+    Listing.new(title: result[0]['title'], address: result[0]['address'], description: result[0]['description'], price: result[0]['price'])
   end
+
 end
--- spec/unit/listing_spec.rb
+++ spec/unit/listing_spec.rb
@@ -2,22 +2,30 @@ require 'listing'
 require 'pg'
 
 describe "Listing" do
-  
   connection = PG.connect(dbname: "makersbnb_test")
 
   describe ".all" do
     it "should display the listings" do
-      
-      listing_1 = connection.exec("INSERT INTO listings(owner_id, address)VALUES ('001', 'test_address_1');")
-      listing_2 = connection.exec("INSERT INTO listings(owner_id, address)VALUES ('002', 'test_address_2');")
-      listing_3 = connection.exec("INSERT INTO listings(owner_id, address)VALUES ('003', 'test_address_3');")
+      listing = Listing.create(title: 'test_title_1', address: 'test_address_1', description: 'test_description_1', price: 'test_price_1')
+      Listing.create(title: 'test_title_2', address: 'test_address_2', description: 'test_description_2', price: 'test_price_2')
+      Listing.create(title: 'test_title_3', address: 'test_address_3', description: 'test_description_3', price: 'test_price_3')
 
       listings = Listing.all
 
-      expect(listings).to include 'test_address_1'      
-      expect(listings).to include 'test_address_2'
-      expect(listings).to include 'test_address_3'
+      expect(listings.length).to eq 3
+      expect(listings.first.title).to eq 'test_title_1'
+      expect(listings.first.address).to eq 'test_address_1'
+      expect(listings.last.title).to eq 'test_title_3'
+    end
+  end
 
+  describe ".create" do
+    it "should create a new instance of listing and add it to the db" do
+      listing = Listing.create(title: 'test_title', address: 'test_address_1', description: 'test_description', price: 'test_price')
+
+      expect(listing).to be_a Listing
+      expect(listing.title).to eq 'test_title'
+      expect(listing.address).to eq 'test_address_1'
     end
   end
 
