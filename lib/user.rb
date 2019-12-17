dcdaaeb466c5889994a6a64d152ca143eabb58c2
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-17 16:54:01 +0000

    Accounted for .start method

--- lib/user.rb
+++ lib/user.rb
@@ -11,6 +11,7 @@ class User
   end
 
   def self.create(name:, username:, email:, password:)
+    DatabaseConnection.start
     encrypted_password = BCrypt::Password.create(password)
     new_user = DatabaseConnection.query("INSERT INTO users(name, email, password, username) VALUES ('#{name}',
     '#{email}','#{encrypted_password}','#{username}') RETURNING name, username, email, id;")
@@ -18,11 +19,13 @@ class User
   end
 
   def self.find(id)
+    DatabaseConnection.start
     found_user = DatabaseConnection.query("SELECT name, username, email, id FROM users WHERE id = '#{id}';")
     User.new(found_user[0]['name'], found_user[0]['username'], found_user[0]['email'], found_user[0]['id'])
   end
 
   def self.login(email:, password: )
+    DatabaseConnection.start
     result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}';")
     return unless result.any? && BCrypt::Password.new(result[0]['password']) == password
     User.find(result[0]['id'])
