9194289f4a584ffd065a6756210580260a4785af
Student <student@Makerss-MacBook-Air.local>
2019-12-18 10:55:01 +0000

    You can use user details after registering like acces his id

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,9 @@
 require 'pg'
 require 'bcrypt'
-require_relative '../database_connection_setup'
+
 class User
   attr_reader :name, :username, :email, :id
-  def initialize(name, username, email, id)
+  def initialize(id, name, email, username)
     @name = name
     @username = username
     @email = email
@@ -15,13 +15,15 @@ class User
     encrypted_password = BCrypt::Password.create(password)
     new_user = DatabaseConnection.query("INSERT INTO users(name, email, password, username) VALUES ('#{name}',
     '#{email}','#{encrypted_password}','#{username}') RETURNING name, username, email, id;")
-    User.new(new_user[0]['name'], new_user[0]['username'], new_user[0]['email'], new_user[0]['id'])
+    User.new(new_user[0]['id'], new_user[0]['name'], new_user[0]['email'], new_user[0]['username'])
   end
 
   def self.find(id)
     DatabaseConnection.start
-    found_user = DatabaseConnection.query("SELECT name, username, email, id FROM users WHERE id = '#{id}';")
-    User.new(found_user[0]['name'], found_user[0]['username'], found_user[0]['email'], found_user[0]['id'])
+    return nil unless id
+
+    found_user = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
+    User.new(found_user[0]['id'], found_user[0]['name'], found_user[0]['email'], found_user[0]['username'])
   end
 
   def self.login(email:, password: )
