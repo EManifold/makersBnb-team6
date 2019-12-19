130223fef1a0e34b8e87957daff0b180d3cfc7ce
Daniel <demartinezgatell@gmail.com>
2019-12-19 17:39:15 +0000

    beginning of greying dates

--- lib/booking.rb
+++ lib/booking.rb
@@ -28,4 +28,10 @@ class Booking
     Booking.new(id: found_booking[0]['id'], listing_id: found_booking[0]['listing_id'], user_id: found_booking[0]['user_id'], start_date: found_booking[0]['start_date'], end_date: found_booking[0]['end_date'], no_of_people: found_booking[0]['no_of_people'], total_price: found_booking[0]['total_price'])
   end
 
+  def self.find_used_dates(listing_id)
+    DatabaseConnection.start
+    found_dates = DatabaseConnection.query("SELECT start_date, end_date FROM bookings WHERE listing_id = #{listing_id}")
+    Booking.new(id: found_dates[0]['id'], listing_id: found_dates[0]['lisitng_id'], user_id: found_dates[0]['user_id'], start_date: found_dates[0]['start_date'], end_date: found_dates[0]['end_date'], no_of_people: found_dates[0]['no_of_people'], total_price: found_booking[0]['total_price'] )
+  end
+
 end
